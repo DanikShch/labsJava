38955412cd0c2a176cd363fe5d27f72f14ae4537
DanikShch <147268869+DanikShch@users.noreply.github.com>
2024-02-22 19:39:50 +0300

    Delete src/main/java/org/example/javalab/Dao directory

--- src/main/java/org/example/javalab/Dao/NumberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.javalab.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NumberRepository extends JpaRepository<NumberEntity,Long> {
-}
